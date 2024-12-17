91a6ba4298aeeb4604716f462c5d9d7c14c12590
22520187 <146148909+22520187@users.noreply.github.com>
2024-12-17 17:29:48 +0700

    UI welcome, setup admin. login, signup

---
+++ app/src/main/java/com/example/se215_superfamilyapp/LoginPageActivity.java
@@ -0,0 +1,12 @@
+package com.example.se215_superfamilyapp;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginPageActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loginpage); // Gắn layout vào Activity
+    }
+}
